2344723c3e77abdb334f7aac8b9c956855a642e1
TekhnoSoft <tekhnosoftdevelop@gmail.com>
2021-01-25 08:57:52 -0300

    secret cache control

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -9,6 +9,8 @@ async function tempo(request, response) {
   const subscribersResponseJson = await subscribersResponse.json();
   const bairro = subscribersResponseJson.bairro;
 
+  response.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
+
   response.json({
     date: dynamicDate.toGMTString(),
     bairro: bairro,
