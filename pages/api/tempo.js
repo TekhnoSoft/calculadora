34a45646a99aacca8b9a965f39b1efd357b7fe2d
TekhnoSoft <tekhnosoftdevelop@gmail.com>
2021-01-25 08:52:03 -0300

    secret api

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,17 @@
-function tempo(request, response) {
+async function tempo(request, response) {
+  const apiSecret = process.env.CONVERTKIT_API_SECRET;
   const dynamicDate = new Date();
+
+  const uri = `https://viacep.com.br/ws/${apiSecret}/json/`;
+
+  const subscribersResponse = await fetch(uri);
+
+  const subscribersResponseJson = await subscribersResponse.json();
+  const bairro = subscribersResponseJson.bairro;
 
   response.json({
     date: dynamicDate.toGMTString(),
+    bairro: bairro,
   });
 }
 
