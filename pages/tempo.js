c294cca54b67b6a4f9a8ccb6d6af1933ad7d3199
TekhnoSoft <tekhnosoftdevelop@gmail.com>
2021-01-26 05:19:42 -0300

    pages tempo

---
+++ pages/tempo.js
@@ -0,0 +1,7 @@
+function Tempo() {
+  const dynamicDate = new Date();
+  const dynamicDateString = dynamicDate.toGMTString();
+  return <div>{dynamicDateString} (dinâmico) </div>;
+}
+
+export default Tempo;
