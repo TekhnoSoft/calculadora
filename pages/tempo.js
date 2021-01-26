00c215f5cf315214d714720b89bc694ffa698b85
TekhnoSoft <tekhnosoftdevelop@gmail.com>
2021-01-26 05:27:10 -0300

    pages tempo estatico

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,7 +1,20 @@
-function Tempo() {
+function Tempo(props) {
   const dynamicDate = new Date();
   const dynamicDateString = dynamicDate.toGMTString();
-  return <div>{dynamicDateString} (dinâmico) </div>;
+  return( 
+    <div>{dynamicDateString} (dinâmico) </div>
+    <div>{props.staticDateString} (estático) </div>
+  );
+}
+
+export function getStaticProps() {
+  const staticDate = new Date();
+  const staticDateString = dynamicDate.toGMTString();
+  return {
+    props: {
+      staticDateString,
+    },
+  };
 }
 
 export default Tempo;
